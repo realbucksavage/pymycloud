afae6bd26d8d6434a62e02fc4b1723af69561ed5
Jay <jgodara@sarvika.com>
2019-04-25 13:21:35 +0530

    Changed /owncloud to /pymycloud

--- constants.py
+++ constants.py
@@ -3,8 +3,8 @@ import os
 
 class Const:
     def work_dir(self):
-        path_in_var = os.environ.get("OWNCLOUD_HOME")
-        return path_in_var if path_in_var else "/owncloud"
+        path_in_var = os.environ.get("PYMYCLOUD_HOME")
+        return path_in_var if path_in_var else "/pymycloud"
 
 
 constants = Const()
