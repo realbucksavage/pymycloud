87000853afc8ce1d96845570783c842dd2e95f24
Jay <jgodara@sarvika.com>
2019-04-28 12:36:40 +0530

    Enabled optional file configuration for logging

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import logging
+import logging.config
+import os
 import socketserver
 import threading
 
 from flask import Flask
 from flask_restful import Api
 
-from constants import constants
 from apis.client.api import ClientFileManagementApi, ClientLoginApi
 from apis.client.transmissions import ClientTransmissionApi
+from constants import constants
 from core.sockets import SocketRequestHandler
 
 app = Flask(__name__, static_folder="frontend", static_url_path="/frontend")
@@ -26,8 +28,16 @@ def root_controller():
     return app.send_static_file("src/index.html")
 
 
+def init_logging():
+    config_file_name = f"{constants.work_dir()}/config/logging.conf"
+    if os.path.exists(config_file_name):
+        logging.config.fileConfig(config_file_name)
+    else:
+        logging.basicConfig(filename=f"{constants.work_dir()}/pymycloud.log", level=logging.INFO)
+
+
 if __name__ == "__main__":
-    logging.basicConfig(filename=f"{constants.work_dir()}/pymycloud.log", level=logging.INFO)
+    init_logging()
 
     address = ("0.0.0.0", 9795)
     server = socketserver.TCPServer(address, SocketRequestHandler)
